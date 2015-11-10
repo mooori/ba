5ad24bc73d536be3561f8ccf9140da77252c4239
Moritz Zielke <moritz.zielke@gmail.com>
2015-11-10 15:32:08 +0100

    utility_ListHelpers: moved functions to .hpp (template class)

--- src/utility/ListHelpers.hpp
+++ src/utility/ListHelpers.hpp
@@ -17,14 +17,20 @@ public:
     /**
      * Constructor, just creating "empty object"
      */
-    ListHelpers();
+    ListHelpers() { }
 
     /**
      * Init list by array.
      * @param arr
      * @param size arr's size (since arr is in fact only ptr to arr[0])
      */
-    list<T> init_by_arr(T arr[], size_t size);
+    list<T> init_by_arr(T arr[], size_t size) {
+        list<T> l;
+        for(unsigned int i = 0; i < size; i++) {
+            l.push_back(arr[i]);
+        }
+        return l;
+    }
 };
 
 #endif
