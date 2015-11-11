99b03f8a109a26e0c9afd32f4fa2f6f58a2de0a5
Moritz Zielke <moritz.zielke@gmail.com>
2015-11-11 18:00:20 +0100

    test_util_parser: swtiched to gtest

--- test/utility_parser.cpp
+++ test/utility_parser.cpp
@@ -1,17 +1,15 @@
-#define BOOST_TEST_MODULE dsSolver
-
 #include <list>
-#include <boost/test/unit_test.hpp>
+
+#include "gtest/gtest.h"
 
 #include "../src/utility/Parser.hpp"
 #include "../src/utility/ListHelpers.hpp"
 
 using namespace std;
 
-BOOST_AUTO_TEST_SUITE(utility)
-BOOST_AUTO_TEST_SUITE(utility_parser)
+namespace {
 
-BOOST_AUTO_TEST_CASE(parse_good_graph_int) {
+TEST(ParserInt, GoodGraph) {
     Parser p;
     parsed_graph<int> pg = p.parse_graph_int("g1.txt");
 
@@ -19,27 +17,24 @@ BOOST_AUTO_TEST_CASE(parse_good_graph_int) {
     int arrv[6] = {1, 2, 3, 4, 5, 6};
     list<int> verts_should = ListHelpers<int>()
             .init_by_arr(arrv, sizeof(arrv)/sizeof(arrv[0]));
-    BOOST_TEST(pg.vertices == verts_should);
+    EXPECT_EQ(verts_should, pg.vertices);
 
     // check edges
     pair<int,int> arre[4] = {pair<int,int>(1, 3), pair<int,int>(2, 3),
             pair<int,int>(1, 5), pair<int,int>(3,6)};
     list< pair<int,int> > edges_should = ListHelpers< pair<int,int> >()
             .init_by_arr(arre, sizeof(arre)/sizeof(arre[0]));
-    BOOST_TEST(pg.edges == edges_should);
+    EXPECT_EQ(edges_should, pg.edges);
 }
 
-BOOST_AUTO_TEST_CASE(parse_bad_verts_graph_int) {
+TEST(ParserInt, BadVertsGraph) {
     Parser p;
-    BOOST_CHECK_THROW(p.parse_graph_int("test/graph_bad_verts.txt"),
-            runtime_error);
+    EXPECT_THROW(p.parse_graph_int("test/graph_bad_verts.txt"), runtime_error);
 }
 
-BOOST_AUTO_TEST_CASE(parse_bad_edges_graph_int) {
+TEST(ParserInt, BadEdgesGraph) {
     Parser p;
-    BOOST_CHECK_THROW(p.parse_graph_int("test/graph_bad_edges.txt"),
-            runtime_error);
+    EXPECT_THROW(p.parse_graph_int("test/graph_bad_edges.txt"), runtime_error);
 }
 
-BOOST_AUTO_TEST_SUITE_END()
-BOOST_AUTO_TEST_SUITE_END()
+} // namespace
