ca0c2b6a5ba2942b7c8088d08432b8db1e6aa209
Moritz Zielke <moritz.zielke@gmail.com>
2015-11-11 18:01:11 +0100

    test_main: added main for gtest

---
+++ test/main.cpp
@@ -0,0 +1,11 @@
+#include "gtest/gtest.h"
+
+/**
+ * Put main in separate file which is then linked, to ensure that
+ * RUN_ALL_TESTS is run only once.
+ */
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
