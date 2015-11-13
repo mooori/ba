112aad2a752446025928022ac21feba1bb481bdd
Moritz Zielke <moritz.zielke@gmail.com>
2015-11-13 16:08:44 +0100

    test_util_parser: upd to keep in line with g1.txt

--- test/utility_parser.cpp
+++ test/utility_parser.cpp
@@ -14,14 +14,14 @@ TEST(ParserInt, GoodGraph) {
     parsed_graph<int> pg = p.parse_graph_int("g1.txt");
 
     // check vertices
-    int arrv[6] = {1, 2, 3, 4, 5, 6};
+    int arrv[6] = {0, 1, 2, 3, 4, 5};
     list<int> verts_should = ListHelpers<int>()
             .init_by_arr(arrv, sizeof(arrv)/sizeof(arrv[0]));
     EXPECT_EQ(verts_should, pg.vertices);
 
     // check edges
-    pair<int,int> arre[4] = {pair<int,int>(1, 3), pair<int,int>(2, 3),
-            pair<int,int>(1, 5), pair<int,int>(3,6)};
+    pair<int,int> arre[5] = {pair<int, int>(0, 4), pair<int, int>(1, 3), 
+            pair<int,int>(2, 3), pair<int,int>(1, 5), pair<int,int>(3,5)};
     list< pair<int,int> > edges_should = ListHelpers< pair<int,int> >()
             .init_by_arr(arre, sizeof(arre)/sizeof(arre[0]));
     EXPECT_EQ(edges_should, pg.edges);
