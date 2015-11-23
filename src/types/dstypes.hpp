4816b5506000866716ea556d86db1f42a935b1ed
Moritz Zielke <moritz.zielke@gmail.com>
2015-11-23 11:25:24 +0100

    types_dstypes: added

---
+++ src/types/dstypes.hpp
@@ -0,0 +1,37 @@
+#ifndef DSTYPES_H
+#define DSTYPES_H
+
+#include <boost/config.hpp>
+#include <boost/graph/adjacency_list.hpp>
+
+/** @brief Definition of some commonly used types */
+
+/**
+ * @note Custom interior vertex properties
+ * Custom interior vertex properties are added according to example in 
+ * libs/graph/example/interior_property_map.cpp
+ * This was the only hint I found on how to do it.
+ */
+
+/** interior vertex prop: original_ida*/
+enum vertex_original_id_t { vertex_original_id };
+namespace boost {
+    BOOST_INSTALL_PROPERTY(vertex, original_id);
+}
+
+typedef boost::adjacency_list<boost::setS, boost::vecS, boost::undirectedS,
+        boost::property<vertex_original_id_t, int> > DSGraph;
+
+typedef int DSVertex;
+typedef std::pair<int, int> DSEdge;
+
+typedef typename boost::graph_traits<DSGraph>::edge_iterator DS_edge_it_t;
+typedef typename boost::graph_traits<DSGraph>::vertex_iterator DS_vertex_it_t;
+
+typedef typename boost::graph_traits<DSGraph>::adjacency_iterator
+        DS_adjacency_it;
+
+typedef typename boost::property_map<DSGraph, vertex_original_id_t>::type
+        DSProp_Acc_original_id;
+
+#endif
