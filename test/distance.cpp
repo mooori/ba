e106e409aaf7dff1106211ccb2b1c882bbe26d11
Moritz Zielke <moritz.zielke@gmail.com>
2016-01-05 17:05:31 +0100

    distance_test: added

---
+++ test/distance.cpp
@@ -0,0 +1,31 @@
+#include <list>
+
+#include <boost/config.hpp>
+#include <boost/graph/adjacency_list.hpp>
+
+#include "gtest/gtest.h"
+
+#include "../src/types/dstypes.hpp"
+#include "../src/types/distance_info.hpp"
+#include "../src/Distance.hpp"
+#include "../src/utility/Parser.hpp"
+
+namespace {
+
+TEST(Distance, getInfo) {
+    // construct graph
+    parsed_graph<int> pg = Parser().parse_graph_int("test/g_distance.txt");
+    DSGraph g(pg.vertices.size());
+    for(std::list< std::pair<int, int> >::iterator it = pg.edges.begin();
+            it != pg.edges.end(); ++it) {
+        DSEdge e = *it;
+        boost::add_edge(e.first, e.second, g);
+    }
+
+    distance_info di = Distance<DSGraph>().get_info(g);
+
+    EXPECT_EQ(3, di.radius);
+    EXPECT_EQ(6, di.diameter);
+}
+
+} // namespace
