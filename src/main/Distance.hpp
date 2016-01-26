c927851ad67e65afadaf4261ee8df1b128a87fa7
Moritz Zielke <moritz.zielke@gmail.com>
2016-01-26 17:37:54 +0100

    Distance: added vert_id_map to work with new graph

--- src/main/Distance.hpp
+++ src/main/Distance.hpp
@@ -1,8 +1,9 @@
 #ifndef DISTANCE_H
 #define DISTANCE_H
 
-#include <vector>
 #include <algorithm>
+#include <map>
+#include <vector>
 
 #include <boost/config.hpp>
 #include <boost/graph/visitors.hpp>
@@ -35,6 +36,36 @@ public:
     distance_info get_info(DSGraph& dsg) {
         distance_info di;
 
+        // edge cases
+        if(dsg.num_vertices() == 0 || dsg.num_vertices() == 1) {
+            di.radius = 0; di.diameter = 0;
+            return di;
+        }
+
+        std::pair<BEdge_it, BEdge_it> eit = dsg.edges();
+        if(dsg.num_vertices() >= 2 && eit.first == eit.second) {
+            di.radius = (std::numeric_limits<int>::max)();
+            di.diameter = (std::numeric_limits<int>::max)();
+            return di;
+        }
+
+        // make boost map, mapping vertex id's to ints in 
+        // [0, dsg.num_vertices()] (required for bfs)
+        typedef std::map<BVertex, int> vid_map_t;
+        vid_map_t vid_map;
+        std::pair<BVertex_it, BVertex_it> vit = dsg.vertices();
+        boost::associative_property_map< std::map<BVertex, int> >
+                boost_vid_map(vid_map);
+        int i = 0;
+        for( ; vit.first != vit.second; ++vit.first) {
+            std::pair<vid_map_t::iterator, bool> res =
+                    vid_map.insert(std::pair<BVertex, int>(*vit.first, i));
+            if(!res.second) {
+                throw std::runtime_error("failed insert in vertex index map");
+            }
+            ++i;
+        }
+
         // starting from each vertex do bfs to get it's eccentricity
         // upd graph's radius and diameter, if necessary
         for(std::pair<BVertex_it, BVertex_it> vit = dsg.vertices();
@@ -42,8 +73,10 @@ public:
             std::map<BVertex, int> dists_src;
             dists_src[*vit.first] = 0;
             int ecc_i = constants::distance_init;
-            boost::breadth_first_search(*(dsg.G), *vit.first, boost::visitor(
-                    distance_bfs< std::map<BVertex, int> >(dists_src, ecc_i)));
+            boost::breadth_first_search(*(dsg.G), *vit.first, 
+                    boost::visitor(distance_bfs< std::map<BVertex, int> >
+                    (dists_src, ecc_i)).    // bgl_named_param notation
+                    vertex_index_map(boost_vid_map));
             di.radius = std::min(di.radius, ecc_i);
             di.diameter = std::max(di.diameter, ecc_i);
         }
