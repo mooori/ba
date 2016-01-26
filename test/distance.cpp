b1ac5c2294c2b28b88204cc986bf2098cfae5d43
Moritz Zielke <moritz.zielke@gmail.com>
2016-01-26 17:57:05 +0100

    test_distance: added tests for edge cases

--- test/distance.cpp
+++ test/distance.cpp
@@ -13,13 +13,25 @@
 namespace {
 
 TEST(Distance, getInfo) {
-    // construct graph
-    DSGraph dsg = Parser().parse_graph_int("g_distance.txt");
-    
-    distance_info di = Distance().get_info(dsg);
+    // graph with non-trivial rad & diam
+    DSGraph dsg1 = Parser().parse_graph_int("graphs/g_distance.txt");
+    distance_info di1 = Distance().get_info(dsg1);
+    EXPECT_EQ(3, di1.radius);
+    EXPECT_EQ(6, di1.diameter);
 
-    EXPECT_EQ(3, di.radius);
-    EXPECT_EQ(6, di.diameter);
+    // edge case graphs
+    // empty graph
+    DSGraph dsg2 = Parser().parse_graph_int("graphs/empty.txt");
+    distance_info di2 = Distance().get_info(dsg2);
+    EXPECT_EQ(0, di2.radius);
+    EXPECT_EQ(0, di2.diameter);
+
+    // graph consisting of just a single vertex
+    DSGraph dsg3 = Parser().parse_graph_int("graphs/just_a_vertex.txt");
+    distance_info di3 = Distance().get_info(dsg3);
+    EXPECT_EQ(0, di3.radius);
+    EXPECT_EQ(0, di3.diameter);
+
 }
 
 } // namespace
