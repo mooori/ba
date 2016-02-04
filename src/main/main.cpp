8aa53f2ebaef1c98d37b01f23f1e5e16d9a41659
Moritz Zielke <moritz.zielke@gmail.com>
2016-02-04 17:53:36 +0100

    main.cpp: changed to include new stuff

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <set>
 #include <utility>
 
 #include <boost/config.hpp>
@@ -6,30 +7,31 @@
 #include <boost/property_map/property_map.hpp>
 
 #include "Distance.hpp"
+#include "FuncIter.hpp"
 #include "../types/dstypes.hpp"
 #include "../utility/Parser.hpp"
+#include "../utility/setops.hpp"
 
 
 int main() {
-    /*
-    Parser p;
-    parsed_graph<int> pg = p.parse_graph_int(
-            "/home/mo/docs/tu/ba/test/g_distance.txt");
-    DSGraph g(pg.vertices.size());
-    for(std::list< std::pair<int, int> >::iterator it = pg.edges.begin();
-            it != pg.edges.end(); ++it) {
-        std::pair<int, int> e = *it;
-        add_edge(e.first, e.second, g);
-    }
-    //vector<TGraph*> conn_comps = ConnComps<TGraph>().find(g);
-    distance_info di = Distance<DSGraph>().get_info(g);
-    std::cout << "radius = " << di.radius << std::endl;
-    std::cout << "diameter = " << di.diameter << std::endl;
-    return 0;
-    */
 
     Parser p;
     DSGraph dsg =
             p.parse_graph_int("../test/graphs/g1.txt");
+    dsg.add_IVertex(666);
+
+    FuncIter f(3, 4);
+    f.increment();
+    std::cout << "fnumber[0] = " << f.get_digit_val(0) << std::endl;
+    std::cout << "fnumber[1] = " << f.get_digit_val(1) << std::endl;
+    f.increment();
+    f.increment();
+    f.increment();
+    std::cout << "fnumber[0] = " << f.get_digit_val(0) << std::endl;
+    std::cout << "fnumber[1] = " << f.get_digit_val(1) << std::endl;
+
+    FuncIter f2(0, 3);
+    //FuncIter f3(3, 0);
+
     return 0;
 }
