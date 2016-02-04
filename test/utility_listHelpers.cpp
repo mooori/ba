543365ce6e5d7fa3ab02d0670d24391efe6d9e5c
Moritz Zielke <moritz.zielke@gmail.com>
2016-02-04 17:51:19 +0100

    test_util_listHelpers: removed creepy boost func

--- test/utility_listHelpers.cpp
+++ test/utility_listHelpers.cpp
@@ -1,19 +1,17 @@
 #include <list>
 
-#include <boost/assign/list_of.hpp> // to create a list
 #include <gtest/gtest.h>
 
 #include "../src/utility/ListHelpers.hpp"
 
-using namespace std;
-
 namespace {
 
 TEST(ListHelpers, initInt) {
-    list<int> l1, l2;
+    std::list<int> l1, l2;
     int arr[6] = {1,2,3,4,5,6};
     l1 = ListHelpers<int>().init_by_arr(arr, sizeof(arr)/sizeof(arr[0]));
-    l2 = boost::assign::list_of(1)(2)(3)(4)(5)(6);
+    l2.push_back(1); l2.push_back(2); l2.push_back(3);
+    l2.push_back(4); l2.push_back(5); l2.push_back(6);
     EXPECT_EQ(l2, l1);
 }
 
