ea16e9cb751d9d447dfe73eeb3d2546b75d78262
Moritz Zielke <moritz.zielke@gmail.com>
2016-02-09 21:33:16 +0100

    main.cpp: expanded; just pushing to branch

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -9,6 +9,7 @@
 #include "Distance.hpp"
 #include "FuncIter.hpp"
 #include "SubsetGen.hpp"
+#include "WComps.hpp"
 #include "../types/dstypes.hpp"
 #include "../utility/Parser.hpp"
 #include "../utility/setops.hpp"
@@ -28,8 +29,8 @@ int main() {
     f.increment();
     f.increment();
     f.increment();
-    std::cout << "fnumber[0] = " << f.get_digit_val(0) << std::endl;
-    std::cout << "fnumber[1] = " << f.get_digit_val(1) << std::endl;
+    //std::cout << "fnumber[0] = " << f.get_digit_val(0) << std::endl;
+    //std::cout << "fnumber[1] = " << f.get_digit_val(1) << std::endl;
 
     FuncIter f2(6, 3);
     //FuncIter f3(3, 0);
@@ -38,9 +39,35 @@ int main() {
     v.push_back(11); v.push_back(22); v.push_back(33); v.push_back(44);
     SubsetGen<int> sg(v);
     sg.get_subsets(4);
-    sg.print_ssets();
+    //sg.print_ssets();
     sg.get_subsets(3);
-    sg.print_ssets();
+    //sg.print_ssets();
 
+    std::set<BVertex> W;
+    WComps wc(dsg, W);
+    std::list<DSGraph*>* comps = wc.get();
+    WComps::delete_comps_ptrs(comps);
+
+    DSGraph dsg1 = Parser().parse_graph_int("../test/graphs/g1.txt");
+    std::set<BVertex> W1;
+    std::cout << "about to build W\n";
+    W1.insert(dsg1.get_BVertex(1)); W1.insert(dsg1.get_BVertex(4));
+    W1.insert(dsg1.get_BVertex(5));
+    std::cout << "built up W\n";
+    WComps wc1(dsg1, W1);
+    std::list<DSGraph*>* comps1 = wc1.get();
+    WComps::delete_comps_ptrs(comps1);
+
+    DSGraph dsg2 = Parser().parse_graph_int("../test/graphs/g_distance.txt");
+    std::set<BVertex> W2{
+        dsg2.get_BVertex(0), dsg2.get_BVertex(1), dsg2.get_BVertex(4),
+        dsg2.get_BVertex(10), dsg2.get_BVertex(12)
+    };
+    WComps WC2(dsg2, W2);
+    std::list<DSGraph*>* comps2 = WC2.get();
+    WComps::delete_comps_ptrs(comps2);
+    
+
+    std::cout << "falou\n";
     return 0;
 }
