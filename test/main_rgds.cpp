9b05635c1b4485b5bd8a3d361bdbe5b159eb5237
Moritz Zielke <moritz.zielke@gmail.com>
2016-02-18 10:08:20 +0100

    test_main_rgds: choose v by spd_ord

--- test/main_rgds.cpp
+++ test/main_rgds.cpp
@@ -1,10 +1,12 @@
 #include <list>
 #include <set>
+#include <vector>
 
 #include <gtest/gtest.h>
 
 #include "../src/main/DSGraph.hpp"
 #include "../src/main/rgds.hpp"
+#include "../src/main/spd.hpp"
 #include "../src/utility/Parser.hpp"
 #include "../src/utility/helpers.hpp"
 
@@ -18,10 +20,11 @@ namespace {
 void test_rgds(const char* gpath, unsigned int dom_num) {
     typedef std::set<IVertex> setI;
     DSGraph dsg = Parser().parse_graph_int(gpath);
+    std::vector<IVertex> spd_ord = spd::build_order(dsg);
 
     // test D with |D| = dom_num is found
     rgds::result_t res1 = rgds::rgds(dsg, setI(), std::list<setI>(),
-            dom_num, setI());
+            dom_num, setI(), spd_ord);
     EXPECT_EQ(true, res1.second) << gpath;
     EXPECT_TRUE(helpers::is_ds(dsg, res1.first));
 
@@ -29,7 +32,7 @@ void test_rgds(const char* gpath, unsigned int dom_num) {
     // however, if dom_num = 0, this is useless, so return
     if(dom_num == 0) { return; }
     rgds::result_t res2 = rgds::rgds(dsg, setI(), std::list<setI>(),
-            dom_num - 1, setI());
+            dom_num - 1, setI(), spd_ord);
     EXPECT_EQ(false, res2.second) << gpath;
     EXPECT_EQ(setI(), res2.first) << gpath;
 
