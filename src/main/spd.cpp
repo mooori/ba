eec41896bdb6bee4b14bf1c25236b6cde88755fd
Moritz Zielke <moritz.zielke@gmail.com>
2016-02-24 16:56:43 +0100

    main_spd: upd to new function names in spd repo

--- src/main/spd.cpp
+++ src/main/spd.cpp
@@ -13,12 +13,12 @@ std::vector<IVertex> spd::build_order(const DSGraph& dsg) {
     GraphWrapper g_spd;
     for(std::pair<BVertex_it, BVertex_it> v_it = dsg.vertices();
             v_it.first != v_it.second; ++v_it.first) {
-        g_spd.addVertices(dsg.get_IVertex(*v_it.first));
+        g_spd.addVertex(dsg.get_IVertex(*v_it.first));
     }
     for(std::pair<BEdge_it, BEdge_it> e_it = dsg.edges();
             e_it.first != e_it.second; ++e_it.first) {
         IEdge ie = dsg.get_IEdge(*e_it.first);
-        g_spd.addEdges(ie.first, ie.second);
+        g_spd.addEdge(ie.first, ie.second);
     }
 
     // for an explanation of config parameters see
